e13a9ee12f07eaab115941a5f2f7440d70f9820b
alexsincai <alex.sincai@yahoo.co.uk>
2022-05-06 13:32:58 +0300

    id generation function

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+from flask import redirect, url_for
 from datetime import datetime
 import time
 
@@ -6,8 +7,10 @@ def get_current_time():
     return datetime.utcfromtimestamp(int(time.time())).strftime("%Y-%m-%d %H:%M:%S")
 
 
-def generate_id_number(ids):
-    ids = sorted(ids)
-    new_id = ids[-1]
-    print(type(new_id))
-    return new_id
+def generate_id_number(file):
+    return max([int(item.get("id")) for item in file] + [0]) + 1
+
+
+def vote_item(id, function, headers, filename):
+    function(filename, headers, id)
+    return redirect(url_for("display_questions", question_id=id))
